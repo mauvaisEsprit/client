7d8d81ee8377218e6541f88fbe5926c236093fc6
Vladyslav Petrenko <legionvlad@icloud.com>
2025-05-23 22:39:44 +0200

    1.001

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="bg-gray-800 text-white py-4 flex-shrink-0 flex justify-center">
             <div className="container mx-auto text-center">
-                <p>&copy; 2025 My Store. All rights reserved.</p>
+                <p>&copy; 2025 My Store.Vladyslav Petrenko. All rights reserved.</p>
                 <p>Follow us on social media:</p>
                 <div className="flex justify-center space-x-4 mt-2">
                     <a href="#" className="text-white hover:text-gray-400">Facebook</a>
